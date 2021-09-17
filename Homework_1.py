d90c06606b9e61d60a3575f7c638a71e7c27d619
MRuletkin <mruletkin6@gmail.com>
2021-09-17 17:30:16 +0300

    Init

---
+++ Homework_1.py
@@ -0,0 +1,19 @@
+import re
+
+def parse(query: str) -> dict:
+    if '?' in query:
+        query = query.split('?')
+        words = re.split('[=&]', query[1])
+        if words[-1] == '':
+            words.pop()
+        return {words[i]: words[i + 1] for i in range(0, len(words), 2)}
+    else:
+        return {}
+
+if __name__ == '__main__':
+    assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
+    assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
+    assert parse('http://example.com/') == {}
+    assert parse('http://example.com/?') == {}
+    assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
+
